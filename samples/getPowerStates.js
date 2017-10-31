5314597b9fc79cf259babd0ef7b291b632b49792
mitzki <mk@mitzki.de>
2017-10-31 22:58:25 +0100

    eslint fixes in samples

--- samples/getPowerStates.js
+++ samples/getPowerStates.js
@@ -1,34 +1,33 @@
 {
-	const EnergenieMagic = require('../src/EnergenieMagic.js');
-	
-	function getArgVByKey(key) {
-		let args = process.argv;
-		for (let arg in args)
-			if (args[arg].trim() === key)
-				if (args.length > parseInt(arg) + 1)
-					return args[parseInt(arg) + 1];
+  const EnergenieMagic = require('../src/EnergenieMagic.js');
+  
+  function getArgVByKey(key) {
+    let args = process.argv;
+    for (let arg in args)
+      if (args[arg].trim() === key)
+        if (args.length > parseInt(arg) + 1)
+          return args[parseInt(arg) + 1];
 
-			if (key == '--port')
-				return 80;
+    if (key == '--port')
+      return 80;
 
-		return '';
-	}
+    return '';
+  }
 
-	if (process.argv.length < 3) {
-		console.error(new Error('NOT_ENOUGH_ARGUMENTS'));
-		return;
-	}
+  if (process.argv.length < 3) {
+    console.error(new Error('NOT_ENOUGH_ARGUMENTS'));
+  } else {
+    const power_stride = new EnergenieMagic({
+      password: getArgVByKey('--password'), // replace
+      host: getArgVByKey('--host'), // replace
+      port: getArgVByKey('--port')
+    });
+    let sockets = {};
 
-	const power_stride = new EnergenieMagic({
-		password: getArgVByKey('--password'), // replace
-		host: getArgVByKey('--host'), // replace
-		port: getArgVByKey('--port')
-	});
-	let sockets = {};
-
-	power_stride.getSockets().then(function(sockets_) {
-        sockets = sockets_;
-      }).catch(function(err) {
-        console.error(new Error(err)); 
-      });
+    power_stride.getSockets().then(function(sockets_) {
+      sockets = sockets_;
+    }).catch(function(err) {
+      console.error(new Error(err)); 
+    });
+  }
 }
--- samples/turnAllSocketsOn.js
+++ samples/turnAllSocketsOn.js
@@ -1,41 +1,40 @@
 {
-	const EnergenieMagic = require('../src/EnergenieMagic.js');
-	
-	function getArgVByKey(key) {
-		let args = process.argv;
-		for (let arg in args)
-			if (args[arg].trim() === key)
-				if (args.length > parseInt(arg) + 1)
-					return args[parseInt(arg) + 1];
+  const EnergenieMagic = require('../src/EnergenieMagic.js');
+  
+  function getArgVByKey(key) {
+    let args = process.argv;
+    for (let arg in args)
+      if (args[arg].trim() === key)
+        if (args.length > parseInt(arg) + 1)
+          return args[parseInt(arg) + 1];
 
-			if (key == '--port')
-				return 80;
+    if (key == '--port')
+      return 80;
 
-		return '';
-	}
+    return '';
+  }
 
-	if (process.argv.length < 3) {
-		console.error(new Error('NOT_ENOUGH_ARGUMENTS'));
-		return;
-	}
-
-	const power_stride = new EnergenieMagic({
-		password: getArgVByKey('--password'),
-		host: getArgVByKey('--host'),
-		port: getArgVByKey('--port')
-	});
-	
-	power_stride.getSockets()
-		.then(function(sockets) {
-			let state = true;
-				for (id in sockets) {
-					power_stride.setSocketState(id, state).then(function(val) {
-						console.log(val);
-						}).catch(function(err) {
-						console.error(new Error(err));
-						});
-				}
-		}).catch(function(err) {
-			console.error(new Error(err)); 
-		});
+  if (process.argv.length < 3) {
+    console.error(new Error('NOT_ENOUGH_ARGUMENTS'));
+  } else {
+    const power_stride = new EnergenieMagic({
+      password: getArgVByKey('--password'),
+      host: getArgVByKey('--host'),
+      port: getArgVByKey('--port')
+    });
+    
+    power_stride.getSockets()
+      .then(function(sockets) {
+        let state = true;
+        for (id in sockets) {
+          power_stride.setSocketState(id, state).then(function(val) {
+            console.log(val);
+          }).catch(function(err) {
+            console.error(new Error(err));
+          });
+        }
+      }).catch(function(err) {
+        console.error(new Error(err)); 
+      });
+  }
 }
