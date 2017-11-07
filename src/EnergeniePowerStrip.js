1002f7b9a4d3e76ebbbaae11cc6449151425c656
mitzki <mk@mitzki.de>
2017-11-08 00:23:31 +0100

    grabbing content outsourced in functions

--- src/EnergeniePowerStrip.js
+++ src/EnergeniePowerStrip.js
@@ -2,17 +2,83 @@
   const url = require('url');
   const EnergenieCore = require('./EnergenieCore');
   
+  /** @type {Enum} Socket Actions */
   const SOCKET_ACTIONS = {
     SET_SOCK_STATE: { PATH: '/energenie.html', METHOD: 'POST' },
     GET_SOCKS: { PATH: '/status.html', METHOD: 'GET' }
   };
   /** EnergenieCore Class */
   let energenieCore;
+  /** Power strip name */
+  let name = '';
   /** Information about sockets on Energenie power strip. */
   let sockets = {};
+  /** Options: host, password & port */
+  let options = {};
   /** Class context */
   let that;
-  
+
+  /**
+   * Grabbing power strip name from header in body tag.
+   * 
+   * @param {String} bodyData <body>(.*)</body>
+   */
+  function grabPowerStripName(bodyData) {
+    let regexStripName = /<div id="header"><h2>(.*?)<\/h2><\/div>/;
+    let regexStripNameContent = regexStripName.exec(bodyData);
+    if (regexStripNameContent != null)
+      name = regexStripNameContent[1].trim();
+  }
+
+  /**
+   * Grabbing names of sockets of power strip.
+   * 
+   * @param {String} scriptData <script>(.*)</script>
+   */
+  function grabSockNames(scriptData) {
+    let regexSockName = /var sock_names = \[(.*?)\];/;
+    let sockNameContent = regexSockName.exec(scriptData);
+    let sockNames = null;
+    
+    if (sockNameContent === null) {
+      console.error(new Error('SOCKET_NAME_ARRAY_NOT_FOUND'));
+      return null;  
+    }
+
+    /** Adding found sock names */
+    sockNames = sockNameContent[1].split(',');
+    for (let i = 0; i < sockNames.length; i++) {
+      let sockName = sockNames[i].trim();
+      let key = 'cte' + (i+1);
+      if (!sockets[key]) sockets[key] = {};
+      sockets[key]['name'] = 
+        sockName.substr(1, sockName.length - 2).trim();     
+    }
+  }
+
+  /**
+   * Grabbing states of sockets of power strip.
+   * 
+   * @param {String} scriptData <script>(.*)</script>
+   */
+  function grabSockStates(scriptData) {
+    let regexSock = /var ctl = \[(.*?)\];/;
+    let sockContent = regexSock.exec(scriptData);
+    if (sockContent === null) {
+      console.error(new Error('SOCKET_ARRAY_NOT_FOUND'));
+      return null;
+    }
+
+    /** Adding found key & state */
+    let sockStates = sockContent[1].split(',');
+    for (let i = 0; i < sockStates.length; i++) {
+      let key = 'cte' + (i+1);
+      if (!sockets[key]) sockets[key] = {};
+      sockets[key]['key'] = 'cte' + (i+1);
+      sockets[key]['state'] = (sockStates[i] === '1') ? true : false;
+    }
+  }
+
   /**
    * Energenie Magic Class.
    *
@@ -20,7 +86,8 @@
    */
   class EnergeniePowerStrip {
 
-    constructor(options) {
+    constructor(options_) {
+      options = options_;
       energenieCore = new EnergenieCore(options);
       that = this;
     }
@@ -30,15 +97,16 @@
      * 
      * @param {String} id ID of the socket.
      * @param {boolean} state Future state of the socket.
+     * @return {Promise} {boolean} Was changing the socket state, okay?
      */
     setSocketState(id, state) {
       return new Promise((resolve, reject) => {
         that.getSockets().then(() => {
           if (sockets[id]) {
             state = (state == true || state == '1') ? '1' : '0';
-            let postData = id  + '=' + state;
+            let postscriptData = id  + '=' + state;
             energenieCore.sendPostRequest(
-              SOCKET_ACTIONS.SET_SOCK_STATE, postData)
+              SOCKET_ACTIONS.SET_SOCK_STATE, postscriptData)
               .then((val) => {     
                 console.info('Updating ' + id + ' to ' + state + ' finished.');
                 resolve(true);
@@ -55,45 +123,32 @@
      * Getting sock states. Reading response and searching via regex
      * inside the <script></script>-tag for the states and the names 
      * array.
+     * 
+     * @return {Promise} Sockets of power strip.
      */
     getSockets() {
       return new Promise((resolve, reject) => {
         energenieCore.sendPostRequest(SOCKET_ACTIONS.GET_SOCKS, '')
           .then(function(res) {
-            // Extracting sockstates
-            let data = res.substring(
+            /** Script data of index.html */
+            let scriptData = res.substring(
               res.indexOf('<script>') + 8, res.indexOf('</script>'));
-            let regexSock = /var ctl = \[(.*?)\];/;
-            let sockContent = regexSock.exec(data);
-            if (sockContent === null) {
-              reject(new Error('SOCKET_ARRAY_NOT_FOUND'));
-              return;
-            }
+            /** Body data of index.html */
+            let bodyData = res.substring(
+              res.indexOf('<body>') + 6, res.indexOf('</body>')
+            );
             
-            let regexSockName = /var sock_names = \[(.*?)\];/;
-            let sockNameContent = regexSockName.exec(data);
-            if (sockNameContent === null) {
-              reject(new Error('SOCKET_NAME_ARRAY_NOT_FOUND'));
-              return;  
-            }
+            grabSockStates(scriptData);
+            grabSockNames(scriptData);
+            grabPowerStripName(bodyData);
 
-            let sockNames = sockNameContent[1].split(',');
-            for (let sKey in sockNames) {
-              let sockName = sockNames[sKey].trim();
-              sockNames[sKey] = sockName.substr(1, sockName.length - 2).trim();
-            }
+            let socks = {
+              host: options.host,
+              name: name,
+              sockets: sockets
+            };
 
-            let sockStates = sockContent[1].split(',');
-            for (let i = 0; i < sockStates.length; i++) {
-              sockets['cte' + (i+1)] = {
-                key: 'cte' + (i+1),
-                name: sockNames[i],
-                state: (sockStates[i] === '1') ? true : false
-              };
-            }
-
-            console.info(sockets);
-            resolve(sockets);
+            resolve(socks);
           }).catch(function(err) {
             console.error('Getting Sockets failed...');
             console.error(err);
